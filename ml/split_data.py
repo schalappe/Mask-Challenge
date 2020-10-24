2077a5a30e144b739bba7a120be6837a968617ee
schalappe <konehakim@gmail.com>
2020-10-24 19:53:41 +0200

    script for dataset

--- ml/split_data.py
+++ ml/split_data.py
@@ -1,28 +1,21 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import progressbar
-import argparse
 import shutil
 
-# argument parse
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-    help="path to input dataset")
-ap.add_argument("-l", "--label", required=True,
-    help="path to input label")
-args = vars(ap.parse_args())
 
 # extracts from csv file
-train_labels = pd.read_csv(args["label"], header='infer')
+train_labels = pd.read_csv('./train_labels.csv', header='infer')
 
 # progress bar
-widgets = ["Extracting Features: ", progressbar.Percentage(), " ",
-    progressbar.Bar(), " ", progressbar.ETA()]
+widgets = [
+    "Split data: ", progressbar.Percentage(), " ",
+    progressbar.Bar(), " ", progressbar.ETA()
+]
 pbar = progressbar.ProgressBar(maxval=len(train_labels), widgets=widgets).start()
 
-i = 0
-for path in train_labels["image"]:
-    shutil.move("./images/"+path, args["dataset"])
-    i += 1
+# loop over label
+for i, path in enumerate(train_labels["image"]):
+    shutil.move("./images/"+path, './train/')
     pbar.update(i)
-
 pbar.finish()
